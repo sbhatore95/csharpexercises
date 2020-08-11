c10f833dbc2abc695b4c00d0a63c28eaa377b9f2
sbhatore95 <sbhatore95@gmail.com>
2020-08-11 16:36:50 +0530

    First two questions done

---
+++ Q1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Q1
+{
+    class Program
+    {
+        static double findAverage(double[] students, int no_of_st)
+        {   
+            // Sorting the numbers of the students in an descending order
+            for(var i=0;i<no_of_st;i++){
+                for(int j=0;j<no_of_st-1;j++){
+                    double temp;
+                    if(students[i] > students[j]){
+                        temp = students[i];
+                        students[i] = students[j];
+                        students[j] = temp;
+                    }
+                }
+            }
+            double precise_failed = (no_of_st)/10;
+            double failed = Math.Floor(precise_failed); // Number of failed students
+            // failed = failed - ((failed % 2) > 1 ? (failed- 1 - failed%2) : (failed - failed%2); // The integer part of {failed}
+            int count = 0;
+            double passed_marks = 0;
+            // Increase the {passed_marks} till the desired location.
+            int no_of_failed = Convert.ToInt32(failed);
+            while(count != no_of_st - no_of_failed){
+                passed_marks += Convert.ToInt32(students[count]);
+                count++;
+            }
+            double answer; // The final aswer to be calculated
+            answer = passed_marks/Convert.ToDouble(no_of_st); // Caclulation of the final result
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            double[] newlist = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            double answer = Program.findAverage(newlist, 10);
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Q2
+{
+    class Program
+    {
+        private static int returnMax(int first, int second){
+            int ans; // Declaration of ans to be output in the end
+            if(first < 20 || second > 30 || first > 30 || second < 20)
+                ans = 0; // Condition satisfied if the above holds true. Store in ans.
+            else{
+                ans = Math.Max(first, second); // Calculation of max of two numbers. Store in ans.
+            }
+            return ans;
+        }
+        static void Main(string[] args)
+        {
+            var no1 = Convert.ToInt32(Console.ReadLine()); // Read first variable
+            var no2 = Convert.ToInt32(Console.ReadLine()); // Read second variable
+            Console.WriteLine(returnMax(no1, no2));
+        }
+    }
+}
